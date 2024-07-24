7f7d50f688e6385866502ec9848fe4ed4bfe3839
“Henil <henilgajjar08@gmail.com>
2024-07-24 01:21:08 -0400

    Final

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 import os
+import time
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -31,6 +32,7 @@ llm = ChatGroq(model_name="llama-3.1-8b-instant")
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
 ## set up Streamlit 
+st.set_page_config(page_title="AppSageAI", page_icon="🧑‍💻")
 st.title("AppSageAI")
 st.subheader("Your Intelligent Companion for Tracking and Enhancing Application Performance")
 
@@ -41,6 +43,10 @@ name=st.text_input("Enter your Name:")
 jd=st.text_input("Enter Job Description:")
 
 uploaded_file=st.file_uploader("Upload your Resume", type="pdf", accept_multiple_files=False)
+
+def measure_response_time(start_time):
+    end_time = time.time()
+    return end_time - start_time
 
 if name:
     if jd:
@@ -74,167 +80,110 @@ if name:
                 "{context}"
             )
 
-            input_prompt1 = (f'''
-                You are AppSage, an advanced AI developed to help users understand their resume and help them get jobs.
-                You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
-                Please share your professional evaluation on whether the candidate's profile aligns with the role.             
-                Job Description: {jd}\n
-                First greet {name} and then Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.'''
-                "\n\n"
-                "{context}"
-            )
+            input_prompts = [
+                (f'''
+                    You are AppSage, an advanced AI developed to help users understand their resume and help them get jobs.
+                    You are an experienced Technical Human Resource Manager, your task is to review the provided resume against the job description. 
+                    Please share your professional evaluation on whether the candidate's profile aligns with the role.             
+                    Job Description: {jd}\n
+                    First greet {name} and then Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.'''
+                    "\n\n"
+                    "{context}"
+                ),
+                (f'''
+                    You are AppSage, an advanced AI developed to help users understand their resume and help them get jobs.
+                    You are a Technical Human Resource Manager with expertise in all the domains, 
+                    your role is to scrutinize the resume in light of the job description provided. 
+                    Job Description: {jd}\n
+                    First greet {name} and then Share your insights on the candidate's suitability for the role from an HR perspective. 
+                    Additionally, offer advice on enhancing the candidate's skills and identify areas where improvement is needed.'''
+                    "\n\n"
+                    "{context}"
+                ),
+                (f'''
+                    You are AppSage, an advanced AI developed to help users understand their resume and help them get jobs.
+                    You are a skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
+                    your task is to evaluate the resume against the provided job description.
+                    Job Description: {jd}\n
+                    First greet {name} and then share the keywords that are missing and provide recommendations for enhancing the candidate's skills.'''
+                    "\n\n"
+                    "{context}"
+                ),
+                (f'''
+                    You are AppSage, an advanced AI developed to help users understand their resume and help them get jobs.
+                    You are a skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
+                    your task is to evaluate the resume against the provided job description. 
+                    Job Description: {jd}\n
+                    First greet {name} and then provide the percentage of match, missing keywords, and final thoughts.'''
+                    "\n\n"
+                    "{context}"
+                ),
+                (f'''
+                    You are AppSage, an advanced AI developed to help users understand their resume and help them get jobs.
+                    You are a skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
+                    your task is to create an excellent cover letter to the hiring manager based on the resume and job description. 
+                    Job Description: {jd}\n
+                    First greet {name} and then Write a compelling cover letter highlighting relevant technologies and experiences.'''
+                    "\n\n"
+                    "{context}"
+                )
+            ]
 
-            input_prompt2 = (f"""
-                You are AppSage, an advanced AI developed to help users understand their resume and help them get jobs.
-                You are an Technical Human Resource Manager with expertise in all the domains, 
-                your role is to scrutinize the resume in light of the job description provided. 
-                Job Description: {jd}\n
-                First greet {name} and then Share your insights on the candidate's suitability for the role from an HR perspective. 
-                Additionally, offer advice on enhancing the candidate's skills and identify areas where improvement is needed."""
-                "\n\n"
-                "{context}"
-            )
+            buttons = [
+                st.button("Tell Me About the Resume"),
+                st.button("How Can I Improvise my Skills?"),
+                st.button("What are the Keywords that are Missing?"),
+                st.button("Percentage Match"),
+                st.button("Cover Letter")
+            ]
 
-            input_prompt3 = (f"""
-                You are AppSage, an advanced AI developed to help users understand their resume and help them get jobs.
-                You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
-                your task is to evaluate the resume against the provided job description.
-                Job Description: {jd}\n
-                As a Human Resource manager, assess the compatibility of the resume with the role. Give me what are the keywords that are missing
-                First greet {name} and then Also, provide recommendations for enhancing the candidate's skills and identify which areas require further development."""
-                "\n\n"
-                "{context}"
-            )
-
-            input_prompt4 = (f"""
-                You are AppSage, an advanced AI developed to help users understand their resume and help them get jobs.
-                You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
-                your task is to evaluate the resume against the provided job description. 
-                Job Description: {jd}\n
-                give me the percentage of match if the resume matches the job description. First greet {name} and then First the output should come as percentage and then keywords missing and last final thoughts."""
-                "\n\n"
-                "{context}"
-            )
-            
-            input_prompt5 = (f"""
-                You are AppSage, an advanced AI developed to help users understand their resume and help them get jobs.
-                You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
-                your task is to create an excellent cover letter to the hiring manager taking the resume against the provided job description. 
-                Job Description: {jd}\n
-                Write me a very good cover letter highlighting all the necessary technologies used during the experience or projects. First paragraph should leave the readers on hook and then following paragraphs should mention what the person brings to the table.
-                First greet {name} and then give the cover letter."""
-                "\n\n"
-                "{context}"
-            )
-
-            submit1 = st.button("Tell Me About the Resume")
-
-            submit2 = st.button("How Can I Improvise my Skills?")
-
-            submit3 = st.button("What are the Keywords that are Missing?")
-
-            submit4 = st.button("Percentage Match")
-            
-            submit5 = st.button("Cover Letter")
+            query = [
+                "Tell Me About the Resume",
+                "How Can I Improvise my Skills?",
+                "What are the Keywords that are Missing?",
+                "Percentage Match",
+                "Cover Letter",
+            ]
 
             user_input = st.text_input("Your question:")
 
 
-            if submit1:
-                qa_prompt = ChatPromptTemplate.from_messages(
-                [
-                    ("system", input_prompt1),
-                    ("human", "{input}"),
-                ]
-                )
-                question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
-                rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+            for i in range(5):
+                if buttons[i]:
+                    start_time = time.time()
+                    qa_prompt = ChatPromptTemplate.from_messages(
+                        [
+                            ("system", input_prompts[i]),
+                            ("human", "{input}")
+                        ]
+                    )
+                    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+                    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
-                response = rag_chain.invoke(
-                    {"input": "Tell Me About the Resume"},
-                )
-                
-                st.write("AppSageAI:", response['answer'])
+                    response = rag_chain.invoke({"input": query[i]})
+                    response_time = measure_response_time(start_time)
 
-            if submit2:
-                qa_prompt = ChatPromptTemplate.from_messages(
-                [
-                    ("system", input_prompt2),
-                    ("human", "{input}"),
-                ]
-                )   
-                question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
-                rag_chain = create_retrieval_chain(retriever, question_answer_chain)
-
-                response = rag_chain.invoke(
-                    {"input": "How Can I Improvise my Skills?"},
-                )
-                
-                st.write("AppSageAI:", response['answer'])
-
-            if submit3:
-                qa_prompt = ChatPromptTemplate.from_messages(
-                [
-                    ("system", input_prompt3),
-                    ("human", "{input}"),
-                ]
-                )   
-                question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
-                rag_chain = create_retrieval_chain(retriever, question_answer_chain)
-
-                response = rag_chain.invoke(
-                    {"input": "What are the Keywords That are Missing?"},
-                )
-                
-                st.write("AppSageAI:", response['answer'])
-
-            if submit4:
-                qa_prompt = ChatPromptTemplate.from_messages(
-                [
-                    ("system", input_prompt4),
-                    ("human", "{input}"),
-                ]
-                )   
-                question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
-                rag_chain = create_retrieval_chain(retriever, question_answer_chain)
-
-                response = rag_chain.invoke(
-                    {"input": "Percentage match"},
-                )
-                
-                st.write("AppSageAI:", response['answer'])
-
-            if submit5:
-                qa_prompt = ChatPromptTemplate.from_messages(
-                [
-                    ("system", input_prompt4),
-                    ("human", "{input}"),
-                ]
-                )   
-                question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
-                rag_chain = create_retrieval_chain(retriever, question_answer_chain)
-
-                response = rag_chain.invoke(
-                    {"input": "Write me a Cover Letter"},
-                )
-                
-                st.write("AppSageAI:", response['answer'])
+                    st.success(f"Response time: {response_time:.2f} seconds")
+                    st.write()
+                    st.write("AppSageAI:", response['answer'])
 
             if user_input:
+                start_time = time.time()
                 qa_prompt = ChatPromptTemplate.from_messages(
-                [
-                    ("system", system_prompt),
-                    ("human", "{input}"),
-                ]
+                    [
+                        ("system", system_prompt),
+                        ("human", "{input}")
+                    ]
                 )
                 question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
                 rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
-                response = rag_chain.invoke(
-                    {"input": user_input},
-                )
-                
-                st.write("AppSageAI:", response['answer']) 
+                response = rag_chain.invoke({"input": user_input})
+                response_time = measure_response_time(start_time)
+
+                st.success(f"Response time: {response_time:.2f} seconds")
+                st.write()
+                st.write("AppSageAI:", response['answer'])
                 
         else:
             st.warning("Upload your Resume!")
